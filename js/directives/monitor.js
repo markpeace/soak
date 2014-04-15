d24e38bf800e46c0f1eb0f90c84c0d4291f74817
markpeace <mark@learnsomestuff.com>
2014-04-15 17:40:53 +0000

    moved brew-monitor-controller to directive

--- js/directives/monitor.js
+++ js/directives/monitor.js
@@ -7,5 +7,32 @@ brewbox.directive('brewMonitorComponent', function() {
 });
 
 brewbox.controller('BrewMonitorComponent', function($scope, ParseService, $ionicSideMenuDelegate) { 
-    console.log("hp")
+    
+    var polarToCartesian = function (centerX, centerY, radius, angleInDegrees) {
+        var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+        return {
+            x: centerX + (radius * Math.cos(angleInRadians)),
+            y: centerY + (radius * Math.sin(angleInRadians))
+        };
+    }
+
+    $scope.describeArc = function(x, y, radius, startAngle, endAngle){
+
+        var start = polarToCartesian(x, y, radius, endAngle);
+        var end = polarToCartesian(x, y, radius, startAngle);
+
+        var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+        var d = [
+            "M", start.x, start.y, 
+            "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+        ].join(" ");
+
+        return d;       
+    }
+
+    
+    $scope.r=[1,2,3]
+   
 });
