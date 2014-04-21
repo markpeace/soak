8ed96e994e713aff87bbac3edf7c14418c9feb8b
markpeace <mark@learnsomestuff.com>
2014-04-21 18:12:12 +0000

    fixed svg bug

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 var brewbox = angular.module('brewbox', ['ionic'])
+
+angular.forEach(['d'], function(name) {
+  var ngName = 'ng' + name[0].toUpperCase() + name.slice(1);
+  brewbox.directive(ngName, function() {
+    return function(scope, element, attrs) {
+      attrs.$observe(ngName, function(value) {
+        attrs.$set(name, value); 
+      })
+    };
+  });
+});
