3c05ad8a6bbf57a6ef39a627215b1dd0bcfb91e3
markpeace <mark@learnsomestuff.com>
2014-05-25 15:03:37 +0000

    fixed header menus

--- js/controllers/mainUI.js
+++ js/controllers/mainUI.js
@@ -1,9 +1,9 @@
 brewbox.controller('MainUI', function($scope, ParseService, $ionicSideMenuDelegate) { 
-    $scope.showLeft = function() {
-        $ionicSideMenuDelegate.toggleLeft();
-    };
-    $scope.showRight = function() {
-        $ionicSideMenuDelegate.toggleRight();
-    };
-    
+        $scope.showLeft = function() {
+                $ionicSideMenuDelegate.toggleLeft();
+        };
+        $scope.showRight = function() {
+                $ionicSideMenuDelegate.toggleRight();
+        };
+
 });
--- js/routes.js
+++ js/routes.js
@@ -1,28 +1,29 @@
 brewbox.config(function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
+        $stateProvider
 
-    .state('ui', {
-        url: '/ui',
-        abstract: true,
-        templateUrl: 'pages/mainUI.html'
-    })
-    .state('ui.brewday', {
-        url: '/brewday/',
-        views: {
-            'centre-panel': {
-                templateUrl: 'pages/monitor.html',
-                controller: 'Monitor'
-            },
-            'left-panel': {
-                templateUrl: 'pages/menu.html'
-            },'right-panel': {
-                templateUrl: 'pages/steps.html',
-                    controller: 'Steps'
-            }
-        }
-    })
+        .state('ui', {
+                url: '/ui',
+                abstract: true,
+                controller:'MainUI',
+                templateUrl: 'pages/mainUI.html'
+        })
+        .state('ui.brewday', {
+                url: '/brewday/',
+                views: {
+                        'centre-panel': {
+                                templateUrl: 'pages/monitor.html',
+                                controller: 'Monitor'
+                        },
+                        'left-panel': {
+                                templateUrl: 'pages/menu.html'
+                        },'right-panel': {
+                                templateUrl: 'pages/steps.html',
+                                controller: 'Steps'
+                        }
+                }
+        })
 
-    $urlRouterProvider.otherwise("/ui/brewday/");
+        $urlRouterProvider.otherwise("/ui/brewday/");
 
 })
