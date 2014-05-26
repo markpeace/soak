d7dee7efca78173cd3ed01358a0ac2e02d047e68
markpeace <mark@learnsomestuff.com>
2014-05-26 14:20:33 +0000

    mocked out recipes

--- js/controllers/recipes.js
+++ js/controllers/recipes.js
@@ -1,8 +1,18 @@
 brewbox.controller('Recipes', function($scope, $http) { 
         console.log("recipes");        
 
-    	//http://ordeal-dromic.codio.io:3000/scrape/https/www.brewtoad.com/users/39308/recipes/
-    
+    	$scope.recipes = [
+            { id: 1, name: 'Plekk', style: 'Imperial IPA'  },
+            { id: 1, name: 'Nimbus Imperius', style: 'Russian Imperial Stout'  },
+            { id: 1, name: 'Stratus', style: 'Pale Ale'  },
+            { id: 1, name: 'Arcus', style: 'Vienna Lager'  },
+            { id: 1, name: 'Lenticular', style: 'India Pale Ale'  }
+        ]
+
+
+
+
+        /*    
         $http({method: 'GET', url: 'http://ordeal-dromic.codio.io:3000/scrape/https/www.brewtoad.com/users/39308/recipes/' })
         .success(function(result) {        
            		console.log ("done")
@@ -11,6 +21,6 @@ brewbox.controller('Recipes', function($scope, $http) {
         error(function(data, status, headers, config) {
             console.log("error")
                 console.log(status)
-        });
+        });*/
 
 });
