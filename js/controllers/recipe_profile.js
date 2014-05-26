145857da2dd28a57624cdd2106fa041b3f11f4ea
markpeace <mark@learnsomestuff.com>
2014-05-26 15:35:51 +0000

    basic recipe info

--- js/controllers/recipe_profile.js
+++ js/controllers/recipe_profile.js
@@ -1,5 +1,12 @@
 brewbox.controller('RecipeProfile', function($scope, $stateParams) { 
 
-        $scope.selectedID=$stateParams.recipe_id
+        if ($stateParams.recipe_id) {
+                $scope.selectedID=$stateParams.recipe_id
+                new Parse.Query(Parse.Object.extend("Recipe"))
+                .get($scope.selectedID).then(function(result) {
+                        $scope.recipe = result
+                        $scope.$apply()
+                })
+        }
 
 });
