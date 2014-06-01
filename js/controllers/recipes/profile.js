e5ed33b52891c77f7f2431301ca248b1a20d1dad
markpeace <mark@learnsomestuff.com>
2014-06-01 20:12:59 +0000

    basic brewday scheduling

--- js/controllers/recipes/profile.js
+++ js/controllers/recipes/profile.js
@@ -1,13 +1,33 @@
 brewbox.controller('RecipeProfile', function($scope, $stateParams, $ionicModal) { 
 
-        if ($stateParams.recipe_id) {
+
+        $scope.moment=moment
+        
+        getRecipe = function() {
+
                 $scope.selectedID=$stateParams.recipe_id
                 new Parse.Query(Parse.Object.extend("Recipe"))
                 .get($scope.selectedID).then(function(result) {
                         $scope.recipe = result
+                        getBrewdays()
+                })
+
+        }
+
+        $scope.brewdays=[]
+        getBrewdays = function () {
+                new Parse.Query(Parse.Object.extend("Brewday"))
+                .equalTo("recipe", $scope.recipe)
+                .descending("date")
+                .find().then(function (result) {
+                        $scope.brewdays=result                        
                         $scope.$apply()
                 })
+
         }
+
+
+        if ($stateParams.recipe_id) getRecipe()
 
 
         $ionicModal.fromTemplateUrl('pages/schedule/new.html', function($ionicModal) {
@@ -16,11 +36,24 @@ brewbox.controller('RecipeProfile', function($scope, $stateParams, $ionicModal) 
                 scope: $scope,
                 animation: 'slide-in-up'
         });   
-        
-        $scope.newBrewdayDate = moment(new Date()).add('days', 1).utc().hour(10).minute(00);     
+
+        $scope.newBrewdayDate = {date: (moment(new Date()).add('days', 1).utc().hour(10).minute(00)._d)}
 
         $scope.saveBrewday = function () {
-                console.log("Save")
+
+                var brewday = new (Parse.Object.extend("Brewday"))().save({
+                        date:$scope.newBrewdayDate.date,
+                        recipe: $scope.recipe
+                }).then(function(result) {
+
+                        $scope.recipe.relation("brewdays").add(result)
+                        $scope.recipe.save().then(function() {
+                                getRecipe()
+                                $scope.newBrewday.hide()  
+                        })
+
+                });
+
         }
 
 });
