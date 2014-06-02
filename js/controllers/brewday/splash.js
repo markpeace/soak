53ab3b07fac12d47b39307d26a9859b514d0dc50
markpeace <mark@learnsomestuff.com>
2014-06-02 19:10:13 +0000

    started to mock out check if brew exists

--- js/controllers/brewday/splash.js
+++ js/controllers/brewday/splash.js
@@ -1,3 +1,23 @@
 brewbox.controller('Splash', function($scope) { 
-        console.log("splash")
+
+        new Parse.Query(Parse.Object.extend("Recipe"))
+        .equalTo
+        
+        var GameScore = Parse.Object.extend("GameScore");
+        var query = new Parse.Query(GameScore);
+        query.equalTo("playerName", "Dan Stemkoski");
+        query.find({
+                success: function(results) {
+                        alert("Successfully retrieved " + results.length + " scores.");
+                        // Do something with the returned Parse.Object values
+                        for (var i = 0; i < results.length; i++) { 
+                                var object = results[i];
+                                alert(object.id + ' - ' + object.get('playerName'));
+                        }
+                },
+                error: function(error) {
+                        alert("Error: " + error.code + " " + error.message);
+                }
+        });                
+
 });
