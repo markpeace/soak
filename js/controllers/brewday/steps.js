c2c70de75ac6964b3050cae195a9a793dbcaac68
markpeace <mark@learnsomestuff.com>
2014-06-06 11:33:24 +0000

    added more step functionality

--- js/controllers/brewday/steps.js
+++ js/controllers/brewday/steps.js
@@ -8,116 +8,126 @@ brewbox.controller('Steps', function($scope, HardwareInterface, $stateParams, $s
                 .find().then(function(result) {
                         if(result.length==0) {$state.go("ui.splash")}
                         $state.brewday=result[0]
-                        if(!result[0].get("steps")) { calculateBrewSteps() }
+                        if(result[0].get("steps")) { resumeBrewday(); } else { compileBrewParameters(); }
+                        compileBrewParameters();
                 })
 
         }
         getRecipe();
 
 
-        var calculateBrewSteps= function () {
+        var compileBrewParameters = function () {
                 RecipeScraper.updateRecipeXML($state.brewday.get("recipe"))
-        }
 
- 
+                $scope.$watch('$state.brewday.get("recipe").get("xml")', function(val){
+                        recipeParameters = $state.brewday.get("recipe").get("xml")
 
+                        brewParameters={
 
-        brewParameters={
+                                // VARIABLES WHICH CHANGE DEPENDING ON THE RECIPE
+                                MSH_grain_weight: recipeParameters.ingredients.total_fermentables,       // in kg
+                                MSH_temperature: 68,       // in C
+                                MSH_thickness: 2.75,       // in l/kg
+                                MSH_time: 60,              // in mins
+                                MSH_mashout_temp: 75,      // in C
+                                FMT_volume: 21,            // in l
 
-                // VARIABLES WHICH CHANGE DEPENDING ON THE RECIPE
-                MSH_grain_weight: 7,       // in kg
-                MSH_temperature: 68,       // in C
-                MSH_thickness: 2.75,       // in l/kg
-                MSH_time: 60,              // in mins
-                MSH_mashout_temp: 75,      // in C
-                FMT_volume: 21,            // in l
-
-                CPR_hop_weight: 200,       // in g
-                CPR_boiltime:60,           // in mins
-
-
-                // EQUIPMENT PROFILE
-                HLT_deadspace: 1,          // in l
-                HLT_groundwater_temp:10,   // in c
-                HLT_minimum_volume: 20,    // in l - without this, the heat coil wouldn't work
-                MSH_deadspace: 2,          // in l
-                MSH_ambient_temp: 15,      // in c
-                CPR_deadspace: 2,          // in l
-                CPR_evaporationrate: 20,   // in %
-                CPR_shrinkage: 4,          // in %
+                                CPR_hop_weight: recipeParameters.ingredients.total_hops,       // in g
+                                CPR_boiltime:60,           // in mins
 
 
-                //AUTOCALCULATIONS
-                calculate: function () {
-
-                        me = brewParameters
-
-                        //CALCULATE BOIL VOLUME
-
-                        me.CPR_preboil_volume =
-                                me.FMT_volume +
-                                ((me.FMT_volume * (me.CPR_evaporationrate/100)) * (me.CPR_boiltime/60)) +
-                                ((me.CPR_hop_weight/100)*1.5) +               
-                                (me.FMT_volume * (me.CPR_shrinkage/100)) + 
-                                me.CPR_deadspace                                                         
-
-
-                        //CALCULATE MASH VOLUMES                
-                        me.MSH_first_water_volume = 
-                                me.MSH_grain_weight * me.MSH_thickness +
-                                me.MSH_deadspace
-
-                        me.MSH_first_runoff_volume =
-                                me.MSH_first_water_volume - me.MSH_grain_weight
-
-                        me.MSH_third_water_volume = me.CPR_preboil_volume - me.MSH_first_runoff_volume
-
-                        me.MSH_second_water_volume = (me.MSH_third_water_volume-me.MSH_first_runoff_volume)/2
-
-                        me.MSH_third_water_volume = me.MSH_third_water_volume - me.MSH_second_water_volume
-
-                        me.MSH_first_runoff_volume = me.MSH_first_runoff_volume + me.MSH_second_water_volume
-
-
-                        //CALCULATE MASH TEMPERATURES
-                        me.MSH_first_water_temperature = (.41/me.MSH_thickness) * (me.MSH_temperature-me.MSH_ambient_temp) + me.MSH_temperature
-                        me.MSH_second_water_temperature = me.MSH_mashout_temp                
-                        me.MSH_third_water_temperature = me.MSH_mashout_temp
-
-                        //CALCULATE HLT VOLUMES - UP TO HERE...
-
-                        me.HLT_total_water_needed = me.MSH_first_water_volume + me.MSH_second_water_volume + me.MSH_third_water_volume
-
-                        me.HLT_first_water_volume = me.HLT_total_water_needed * .95
-
-                        if (me.HLT_first_water_volume - me.MSH_first_water_volume < me.HLT_minimum_volume ) {
-                                me.HLT_first_water_volume=me.HLT_first_water_volume + (me.HLT_minimum_volume-(me.HLT_first_water_volume - me.MSH_first_water_volume))
+                                // EQUIPMENT PROFILE
+                                HLT_deadspace: 1,          // in l
+                                HLT_groundwater_temp:10,   // in c
+                                HLT_minimum_volume: 20,    // in l - without this, the heat coil wouldn't work
+                                MSH_deadspace: 2,          // in l
+                                MSH_ambient_temp: 15,      // in c
+                                CPR_deadspace: 2,          // in l
+                                CPR_evaporationrate: 20,   // in %
+                                CPR_shrinkage: 4,          // in %
                         }
 
-                        me.HLT_volume_after_strike = me.HLT_first_water_volume - me.MSH_first_water_volume
+                        calculateParameters(brewParameters)                       
 
-                        me.HLT_second_water_volume = 
-                                (me.MSH_first_water_temperature-me.MSH_temperature)/
-                                (me.MSH_first_water_temperature-((me.HLT_volume_after_strike * me.MSH_first_water_temperature) + (1 * me.HLT_groundwater_temp)) / 
-                                 (me.HLT_volume_after_strike+1))
+                }, true);
+
+        }
+
+        calculateParameters=function(brewParameters) {
+                me = brewParameters
+
+                //CALCULATE BOIL VOLUME
+
+                me.CPR_preboil_volume =
+                        me.FMT_volume +
+                        ((me.FMT_volume * (me.CPR_evaporationrate/100)) * (me.CPR_boiltime/60)) +
+                        ((me.CPR_hop_weight/100)*1.5) +               
+                        (me.FMT_volume * (me.CPR_shrinkage/100)) + 
+                        me.CPR_deadspace                                                         
 
 
-                        me.HLT_volume_after_second_addition = me.HLT_volume_after_strike + me.HLT_second_water_volume
+                //CALCULATE MASH VOLUMES                
+                me.MSH_first_water_volume = 
+                        me.MSH_grain_weight * me.MSH_thickness +
+                        me.MSH_deadspace
 
-                        me.HLT_temperature_after_second_addition =
-                                ((me.HLT_volume_after_strike * me.MSH_first_water_temperature) + 
-                                 (me.HLT_second_water_volume * me.HLT_groundwater_temp)) / 
-                                (me.HLT_volume_after_strike+me.HLT_second_water_volume)
+                me.MSH_first_runoff_volume =
+                        me.MSH_first_water_volume - me.MSH_grain_weight
 
-                        me.HLT_waste_water = me.HLT_volume_after_second_addition - (me.MSH_second_water_volume + me.MSH_third_water_volume)                               
+                me.MSH_third_water_volume = me.CPR_preboil_volume - me.MSH_first_runoff_volume
 
-                },
+                me.MSH_second_water_volume = (me.MSH_third_water_volume-me.MSH_first_runoff_volume)/2
 
-                steps: [],
+                me.MSH_third_water_volume = me.MSH_third_water_volume - me.MSH_second_water_volume
 
-                stepTemplate: function () {
+                me.MSH_first_runoff_volume = me.MSH_first_runoff_volume + me.MSH_second_water_volume
+
+
+                //CALCULATE MASH TEMPERATURES
+                me.MSH_first_water_temperature = (.41/me.MSH_thickness) * (me.MSH_temperature-me.MSH_ambient_temp) + me.MSH_temperature
+                me.MSH_second_water_temperature = me.MSH_mashout_temp                
+                me.MSH_third_water_temperature = me.MSH_mashout_temp
+
+                //CALCULATE HLT VOLUMES - UP TO HERE...
+
+                me.HLT_total_water_needed = me.MSH_first_water_volume + me.MSH_second_water_volume + me.MSH_third_water_volume
+
+                me.HLT_first_water_volume = me.HLT_total_water_needed * .95
+
+                if (me.HLT_first_water_volume - me.MSH_first_water_volume < me.HLT_minimum_volume ) {
+                        me.HLT_first_water_volume=me.HLT_first_water_volume + (me.HLT_minimum_volume-(me.HLT_first_water_volume - me.MSH_first_water_volume))
+                }
+
+                me.HLT_volume_after_strike = me.HLT_first_water_volume - me.MSH_first_water_volume
+
+                me.HLT_second_water_volume = 
+                        (me.MSH_first_water_temperature-me.MSH_temperature)/
+                        (me.MSH_first_water_temperature-((me.HLT_volume_after_strike * me.MSH_first_water_temperature) + (1 * me.HLT_groundwater_temp)) / 
+                         (me.HLT_volume_after_strike+1))
+
+
+                me.HLT_volume_after_second_addition = me.HLT_volume_after_strike + me.HLT_second_water_volume
+
+                me.HLT_temperature_after_second_addition =
+                        ((me.HLT_volume_after_strike * me.MSH_first_water_temperature) + 
+                         (me.HLT_second_water_volume * me.HLT_groundwater_temp)) / 
+                        (me.HLT_volume_after_strike+me.HLT_second_water_volume)
+
+                me.HLT_waste_water = me.HLT_volume_after_second_addition - (me.MSH_second_water_volume + me.MSH_third_water_volume)                               
+
+                calculateBrewSteps(me)
+
+
+        }
+
+        calculateBrewSteps = function (brewParameters) {
+                steps=[]
+
+                stepTemplate =function () {
 
                         st=this
+                        
+                        st.isActive=false;
 
                         st.currentValue = 12.23
 
@@ -139,41 +149,37 @@ brewbox.controller('Steps', function($scope, HardwareInterface, $stateParams, $s
 
                                 st.ping = setInterval(st.updateProgress,HardwareInterface.settings.pulseInterval);                                                              
                         }
-                },
-
-                createSteps: function () {
-                        me = brewParameters;
-
-                        angular.forEach([
-                                { 
-                                        title: "Prefill HLT",
-                                        commandPort: 200,
-                                        command: "HLT SET VOL " + me.HLT_first_water_volume,
-                                        targetValue: me.HLT_first_water_volume,
-                                        targetValueUnit: "l",
-                                        hardwareReference: "hlt",
-                                        hardwareVariable: "vol"
-                                }
-                        ], function (s) {
-                                newStep = new me.stepTemplate
-                                angular.forEach(s, function(value,key) { newStep[key]=value })
-
-                                newStep.initialise();
-
-                                me.steps.push(newStep);        
-                        })
-
                 }
+
+
+                me = brewParameters;
+
+                angular.forEach([
+                        { 
+                                title: "Prefill HLT",
+                                trigger: "user",
+                                commandPort: 200,
+                                command: "HLT SET VOL " + me.HLT_first_water_volume,
+                                targetValue: me.HLT_first_water_volume,
+                                targetValueUnit: "l",
+                                hardwareReference: "hlt",
+                                hardwareVariable: "vol"
+                        }
+                ], function (step) {
+                        newStep = new stepTemplate
+                        angular.forEach(step, function(value,key) { newStep[key]=value })
+                        steps.push(newStep);        
+                })
+
+
+                $state.brewday.set("steps", steps).save().then(resumeBrewday)
 
         }
 
 
-        brewParameters.calculate();
-        brewParameters.createSteps();
-
-
-        $scope.currentStep=1;        
-
-        $scope.brewSteps=brewParameters.steps;
+        resumeBrewday=function () {
+                $scope.brewSteps=$state.brewday.get('steps')                   
+                console.log("resumed")
+        }
 
 });
