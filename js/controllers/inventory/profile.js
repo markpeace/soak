1b782dfa2f16b96a9ae1d98a31b0e42c279cc172
markpeace <mark@learnsomestuff.com>
2014-06-22 10:18:18 +0000

    started to set up equivalences

--- js/controllers/inventory/profile.js
+++ js/controllers/inventory/profile.js
@@ -1,16 +1,38 @@
 brewbox.controller('IngredientProfile', function($scope, $stateParams) { 
 
         getIngredient = function() {
-				$scope.selectedID=$stateParams.ingredient_id;
-           
+        	
                 new Parse.Query(Parse.Object.extend("Inventory"))
                 .get($scope.selectedID).then(function(result) {
                         $scope.ingredient = result
-                		$scope.$apply()
+                		getAllIngredients()
                 })
 
         }
-		getIngredient();
+        
+        getAllIngredients = function () {
+        	new Parse.Query(Parse.Object.extend("Inventory"))
+        		.equalTo("type", $scope.ingredient.get("type"))
+        		.notEqualTo("label", $scope.ingredient.get("label"))
+        		.ascending("label")
+                .find().then(function(result) {
+                        $scope.ingredients = result
+                		$scope.$apply()
+                })
+        }
+        
+        
+        if($stateParams.ingredient_id) {
+				$scope.selectedID=$stateParams.ingredient_id;
+			getIngredient();
+		}
 		
+		$scope.addIngredient = function () {
+			alert("add ingredient")
+		}
+		
+		$scope.setEquivalent = function (equivalentID) {
+			console.log(equivalentID)
+		}
 				
 });
