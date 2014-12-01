2ce69623e765656604681637fe1062c7c5c140cc
markpeace <mark@learnsomestuff.com>
2014-12-01 16:05:43 +0000

    mocked out parameters screen

--- js/controllers/active.js
+++ js/controllers/active.js
@@ -1,3 +1,12 @@
 soak.controller('Active', function($scope) { 
         console.log("active")
+        $scope.params = [
+                { hint: 'Wait Time (mins)', value: null },             
+                { hint: 'Hot Time (mins)', value: null },                
+                { hint: 'Cold Time (mins)', value: null }                
+        ]
+        
+        $scope.submit = function () {
+                console.log($scope.params)
+        }
 });
