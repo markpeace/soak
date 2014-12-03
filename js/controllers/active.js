71c274dbf57cc5d00c2745ba5d524507a0e9e7d2
markpeace <mark@learnsomestuff.com>
2014-12-03 16:11:28 +0000

    rationalised alert notices

--- js/controllers/active.js
+++ js/controllers/active.js
@@ -1,5 +1,9 @@
 soak.controller('Active', function($scope) { 
-        console.log("active")
+        console.info("controller handed over to: inactive.js")
+        
+        console.warn("this page needs rewriting to be based on autocalculation of timings based on desired depth and temperature")
+        console.warn("this page needs reformatting to be driven by a drag-and-drop graphical interface")
+
         $scope.params = [
                 { hint: 'Wait Time (mins)', value: null },             
                 { hint: 'Hot Time (mins)', value: null },                
