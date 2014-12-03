1d367d608cf9be0eada560d141c7f91eee1abdc5
markpeace <mark@learnsomestuff.com>
2014-12-03 16:08:19 +0000

    rationalised alert notices

--- js/controllers/initialising.js
+++ js/controllers/initialising.js
@@ -1,13 +1,8 @@
 soak.controller('Intialising', function($state) { 
         
-        console.info("initialising") 
-        console.warn("need to write a function which pings for current hardware state")
-        
-        //PING THE HARDWARE
-        //IF INACTIVE, REDIRECT TO INACTIVE
-        //IF ACTIVE, BUT NO PARAMETERS REDIRECT TO ACTIVE
-        //IF ACTIVE WITH PARAMETERS, REDIRECT TO FILLING 
-        
+        console.info("controller handed over to: initialising.js")
+        console.warn("need to write a function which pings for current hardware state and redirects appropriately. For now, autodirecting to...")
+                
         $state.go("inactive")
        
 });
