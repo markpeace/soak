e911e30375402cb0a1a4a64fb6358887d1288e4e
markpeace <mark@learnsomestuff.com>
2014-12-07 17:46:54 +0000

    automatically redirect if filling

--- js/controllers/inactive.js
+++ js/controllers/inactive.js
@@ -1,12 +1,31 @@
 soak.controller('Inactive', function($scope, $state, HardwareInterface) { 
         console.info("controller handed over to: inactive.js")
-        
-        $scope.buttonText="[ACTIVATE SYSTEM]"
-        
+
+        $scope.buttonText="Connecting to System..."
+
+
+        HardwareInterface.ping().then(function(r) {
+                r=r.data.split(";")
+                if(parseInt(r[0])==1) {
+                        
+                        if(parseInt(r[3])<parseInt(r[6])) {
+                                $state.go("filling")
+                        } else {
+                                $state.go("active")
+                        }
+                        
+                          
+                } else {
+                        $scope.buttonText="[ACTIVATE SYSTEM]"
+                }
+        })
+
+
+
         $scope.activate = function () {
-               
+
                 $scope.buttonText="Activating..."
-                
+
                 HardwareInterface.toggleActivation().then(function(result) {
                         if(result.data==1) {
                                 $scope.buttonText="Activated"
@@ -17,5 +36,5 @@ soak.controller('Inactive', function($scope, $state, HardwareInterface) {
                 })
 
         }
-        
+
 });
